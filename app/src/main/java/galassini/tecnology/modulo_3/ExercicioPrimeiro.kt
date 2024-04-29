2055d03eb230bef97b56930503e8382091f1e872
Priscila Galassini <priscilagalassini@gmail.com>
2024-04-29 11:03:26 -0300

    commit init

---
+++ app/src/main/java/galassini/tecnology/modulo_3/ExercicioPrimeiro.kt
@@ -0,0 +1,12 @@
+package galassini.tecnology.modulo_3
+
+class ExercicioPrimeiro {
+}
+
+fun main() {
+    var numero = 6
+    println("Valor inicial: $numero")
+
+    numero = 5
+    println("Novo valor: $numero")
+}
---
+++ app/src/main/java/galassini/tecnology/modulo_3/ExercicioSegundo.kt
@@ -0,0 +1,16 @@
+package galassini.tecnology.modulo_3
+
+class ExercicioSegundo {
+}
+
+fun main() {
+    val numero = 6
+    println("Valor inicial: $numero")
+
+    numero = 7
+
+    println("Valor após tentativa de alteração: $numero")
+}
+
+//Ao tentar compilar o código, retornou um erro indicando que não é possível reatribuir,
+// um valor a uma variável imutável.
